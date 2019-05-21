1b0ede542d610de310d1c56eff999668d0c2d27f
MinexAutomation <minex.automation@gmail.com>
2019-05-21 16:46:04 -0400

    Initial commit of source stub. Initial version is 0.0.1.

---
+++ source/R5T.NetStandard.IO.Serialization/Code/BinaryFileSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace R5T.NetStandard.IO.Serialization
+{
+    public static class BinaryFileSerializer
+    {
+        public static T Deserialize<T>(string binaryFilePath)
+        {
+            using (var fileStream = FileStreamHelper.NewRead(binaryFilePath))
+            {
+                var output = BinaryStreamSerializer.Deserialize<T>(fileStream);
+                return output;
+            }
+        }
+
+        public static void Serialize<T>(string binaryFilePath, T value, bool overwrite = true)
+        {
+            using (var fileStream = FileStreamHelper.NewWrite(binaryFilePath, overwrite))
+            {
+                BinaryStreamSerializer.Serialize(fileStream, value);
+            }
+        }
+    }
+
+
+    public class BinaryFileSerializer<T> : FileSerializer<T>
+    {
+        public BinaryFileSerializer()
+            : base(new BinaryStreamSerializer<T>())
+        {
+        }
+    }
+}
